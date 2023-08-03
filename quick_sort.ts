e8b1b099ec69401f235bd5846a765b3366cf4a87
Chad Carbert <chadcarbert@me.com>
2023-08-03 11:36:26 -0400

    Add recursive quick sort implementation

---
+++ quick_sort.ts
@@ -0,0 +1,43 @@
+export function recursive(items: number[]): number[] {
+  switch (items.length) {
+    case 0:
+    case 1: {
+      return items;
+    }
+
+    case 2: {
+      const sorted = items[0] > items[1] ? [items[1], items[0]] : items;
+      return sorted;
+    }
+
+    default: {
+      const PIVOT_INDEX = 0;
+      const pivot = items[PIVOT_INDEX];
+      const { before, after } = items.reduce(
+        ({ before, after }, item, index) => {
+          // avoid including pivot by just returning the existing { before, after}
+          // an alternative would be to create a copy of the array
+          // and unshift but that seems more intensive than just
+          // skipping one function call
+          if (index === PIVOT_INDEX) {
+            return { before, after };
+          }
+
+          if (item > pivot) {
+            after.push(item);
+          } else {
+            before.push(item);
+          }
+
+          return { before, after };
+        },
+        { before: [], after: [] } as { before: number[]; after: number[] }
+      );
+
+      const beforeSorted = recursive(before);
+      const afterSorted = recursive(after);
+
+      return [...beforeSorted, pivot, ...afterSorted];
+    }
+  }
+}

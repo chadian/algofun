7f30f1db84aaa5b105ace1a0306b4689ac9d4681
Chad Carbert <chadcarbert@me.com>
2023-08-12 17:54:21 -0400

    Add recursive breadth_first_search and update tests

--- breadth_first_search.ts
+++ breadth_first_search.ts
@@ -1,7 +1,7 @@
 import { Graph, Node } from "./graph.ts";
 import { Queue } from "./queue.ts";
 
-export function search(graph: Graph, start: any, find: any): any[] {
+export function iterative(graph: Graph, start: any, find: any): any[] {
   const startNode = graph.nodeForValue(start);
   const findNode = graph.nodeForValue(find);
 
@@ -56,4 +56,62 @@ export function search(graph: Graph, start: any, find: any): any[] {
 
   const valuePath = path.map(node => node.value);
   return valuePath;
+}
+
+export function recursive(graph: Graph, start: any, find: any): any {
+  const startNode = graph.nodeForValue(start);
+  const findNode = graph.nodeForValue(find);
+
+  if (!startNode) {
+    throw new Error('`start` does not exist in graph');
+  }
+
+  if (!findNode) {
+    throw new Error('`find` does not exist in graph');
+  }
+
+  if (startNode === findNode) {
+    return [startNode.value];
+  }
+
+  const queue = new Queue();
+  queue.enqueue(startNode);
+
+  const nodeToParent = _searchRecursive(queue, findNode);
+  const path = processNodeToParentMap(nodeToParent, findNode);
+  return path;
+}
+
+function processNodeToParentMap(nodeToParent: Map<Node, Node>, current: Node, path: Node[] = []): Node[] {
+  path.unshift(current.value);
+
+  if (!nodeToParent.has(current)) {
+    return path;
+  }
+
+  return processNodeToParentMap(nodeToParent, nodeToParent.get(current)!, path);
+}
+
+// Slightly cheating by using a Map and Queue to track state in the recursive solution
+// Maybe worth revisiting later
+function _searchRecursive(queue: Queue, find: Node, nodeToParent: Map<Node, Node> = new Map()) {
+  if (queue.length === 0) {
+    return nodeToParent;
+  }
+
+  if (nodeToParent.has(find)) {
+    return nodeToParent;
+  }
+
+  const current = queue.dequeue() as Node;
+
+  // add unseen children to map
+  current.edges.forEach(child => {
+    if (!nodeToParent.has(child)) {
+      nodeToParent.set(child, current);
+      queue.enqueue(child);
+    }
+  });
+
+  return _searchRecursive(queue, find, nodeToParent);
 }
--- breadth_first_search_test.ts
+++ breadth_first_search_test.ts
@@ -1,46 +1,51 @@
 import { assertThrows } from 'https://deno.land/std@0.195.0/assert/assert_throws.ts';
-import { search } from './breadth_first_search.ts';
+import { iterative, recursive } from './breadth_first_search.ts';
 import { Graph } from "./graph.ts";
 import { assertEquals } from "https://deno.land/std@0.195.0/testing/asserts.ts";
 
-Deno.test("it throws an error when `start` argument is not in the graph", function() {
-  const graph = new Graph();
-  const find = 'find';
-  graph.createNode(find);
+function breadthFirstSearchTests(search: (graph: Graph, start: any, find: any) => any[], label: string) {
+  Deno.test(`[${label}] it throws an error when \`start\` argument is not in the graph`, function() {
+    const graph = new Graph();
+    const find = 'find';
+    graph.createNode(find);
 
-  assertThrows(() => search(graph, 'start', find));
-});
+    assertThrows(() => search(graph, 'start', find));
+  });
 
-Deno.test("it throws an error when `find` argument is not in the graph", function() {
-  const graph = new Graph();
-  const start = 'start';
-  graph.createNode(start);
+  Deno.test(`[${label}] it throws an error when \`find\` argument is not in the graph`, function() {
+    const graph = new Graph();
+    const start = 'start';
+    graph.createNode(start);
 
-  assertThrows(() => search(graph, start, 'find'));
-});
+    assertThrows(() => search(graph, start, 'find'));
+  });
 
-Deno.test("it returns an array with the `find` item when `start` and `find` are the same", function() {
-  const graph = new Graph();
-  graph.createNode('same');
-  const path = search(graph, 'same', 'same');
+  Deno.test(`[${label}] it returns an array with the \`find\` item when \`start\` and \`find\` are the same`, function() {
+    const graph = new Graph();
+    graph.createNode('same');
+    const path = search(graph, 'same', 'same');
 
-  assertEquals(path, ['same']);
-});
+    assertEquals(path, ['same']);
+  });
 
-Deno.test('it can find the shortest path between `start` and `find`', function() {
-  const graph = new Graph();
+  Deno.test(`[${label}] it can find the shortest path between \`start\` and \`find\``, function() {
+    const graph = new Graph();
 
-  // graph based on example from the book
-  graph.connect({ from: 'cab', to: 'car' });
-  graph.connect({ from: 'cab', to: 'cat' });
-  graph.connect({ from: 'cat', to: 'mat' });
-  graph.connect({ from: 'mat', to: 'bat' });
-  graph.connect({ from: 'cat', to: 'bat' });
-  graph.connect({ from: 'car', to: 'bar' });
-  graph.connect({ from: 'bar', to: 'bat' });
+    // graph based on example from the book
+    graph.connect({ from: 'cab', to: 'car' });
+    graph.connect({ from: 'cab', to: 'cat' });
+    graph.connect({ from: 'cat', to: 'mat' });
+    graph.connect({ from: 'mat', to: 'bat' });
+    graph.connect({ from: 'cat', to: 'bat' });
+    graph.connect({ from: 'car', to: 'bar' });
+    graph.connect({ from: 'bar', to: 'bat' });
 
-  const path = search(graph, 'cab', 'bat');
+    const path = search(graph, 'cab', 'bat');
 
-  // page 104 for illustration
-  assertEquals(path, ['cab', 'cat', 'bat']);
-});
+    // page 104 for illustration
+    assertEquals(path, ['cab', 'cat', 'bat']);
+  });
+}
+
+breadthFirstSearchTests(iterative, 'iterative');
+breadthFirstSearchTests(recursive, 'recursive');
